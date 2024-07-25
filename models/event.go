925b1b99058aee4273c2c2db7e683d9e463a2558
Kostas Chikimtzis <kostaschikimtzis@Kostass-Air.router.wind>
2024-07-25 13:11:00 +0300

    added create event route and handler

--- models/event.go
+++ models/event.go
@@ -4,16 +4,20 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding: required`
+	Description string    `binding: required`
+	Location    string    `binding: required`
+	DateTime    time.Time `binding: required`
 	UserID      int
 }
 
 var events = []Event{}
 
 func (e Event) Save() {
-	// test
+	// later: add it to database
 	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
 }
